cede978b982698c0d0597475306381b8029660d6
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-08-30 13:04:46 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\-\'\s]/g, "");
+  }
+  static titleize(string) {
+    const exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const words = string.toLowerCase().split(" ");
+
+    const capitalizedWords = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+
+    return capitalizedWords.join(" ");
+  }
 }
